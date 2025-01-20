923c8d498833f65950eb593ae8706143c6b072a9
shruti <you@example.com>
2025-01-20 20:01:43 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://shrutideshmukh905:abcefg@cluster0.t4edv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
